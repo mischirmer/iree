mischirmer/iree

commit df73163cec54fcbaa5bcaea6fa9a0124d181b8ca

Microkernel for `linalg_ext.pack`, take 2 (#10969)
This is the re-landing of #10927, which was rolled back in #10951 due to
GCC breakage.

Plus:
- The fix for the GCC fix that caused it to be reverted. It
was caused by a difference betwen G++ and Clang++: G++ defines
`__STDC__`, Clang++ does not.
- Lit-tests. I had inadvertently
forgotten that. This actually caught an issue, I had defined the type of
`padding_value` as `i8` in the `vmvx.pack.i8i8` import and that was a
mistake as narrow integers are always `i32` at that level. The reason
why the e2e tests hadn't caught that is that they are only testing
`pack.i32i32`.

runtime/src/iree/builtins/ukernel/static_assert.h
@@ -1,0 +1,32 @@
+// Copyright 2022 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_BUILTINS_UKERNEL_STATIC_ASSERT_H_
+#define IREE_BUILTINS_UKERNEL_STATIC_ASSERT_H_
+
+// IREE_UKERNEL_STATIC_ASSERT, a static assert macro usable in C, C++ and
+// assembly (though it may evaluate to nothing in assembly).
+
+#if defined(__ASSEMBLER__)
+// Note that __STDC__ may also be defined here (when the assembler driver is the
+// C compiler driver). So it's easiest to handle __ASSEMBLER__ before __STDC__.
+//
+// Evaluate to nothing (TODO: If we care for static asserts in assembly, perhaps
+// implement them as an assembler .macro?)
+#define IREE_UKERNEL_STATIC_ASSERT(COND)
+#elif defined(__cplusplus)
+// Note that depending on the C++ compiler, __STDC__ may or may not be defined
+// here! It is defined by G++ but not by Clang++! So, let's also handle
+// __cplusplus before __STDC__.
+#define IREE_UKERNEL_STATIC_ASSERT(COND) static_assert(COND, #COND)
+#elif defined(__STDC__)
+// Really C, as neither __ASSEMBLER__ nor __cplusplus are defined.
+#define IREE_UKERNEL_STATIC_ASSERT(COND) _Static_assert(COND, #COND)
+#else
+#error Expected either __cplusplus or __STDC__ or __ASSEMBLER__ to be defined.
+#endif
+
+#endif  // IREE_BUILTINS_UKERNEL_STATIC_ASSERT_H_

compiler/src/iree/compiler/Codegen/VMVX/BUILD
@@ -28,7 +28,8 @@ iree_compiler_cc_library(
         "//compiler/src/iree/compiler/Dialect/VMVX/IR",
         "//compiler/src/iree/compiler/Dialect/VMVX/IR:VMVXDialect",
         "//compiler/src/iree/compiler/Utils",
-        "//runtime/src/iree/builtins/ukernel",
+        "//llvm-external-projects/iree-dialects:IREELinalgExtDialect",
+        "//runtime/src/iree/builtins/ukernel:exported_flag_bits",
         "@llvm-project//llvm:Support",
         "@llvm-project//mlir:ArithDialect",
         "@llvm-project//mlir:BufferizationDialect",

compiler/src/iree/compiler/Codegen/VMVX/CMakeLists.txt
@@ -19,6 +19,7 @@ iree_cc_library(
     "VMVXAssignConstantOrdinals.cpp"
     "VMVXLinkExecutables.cpp"
   DEPS
+    IREELinalgExtDialect
     LLVMSupport
     MLIRArithDialect
     MLIRBufferizationDialect
@@ -28,7 +29,7 @@ iree_cc_library(
     MLIRMemRefDialect
     MLIRPass
     MLIRTransforms
-    iree::builtins::ukernel
+    iree::builtins::ukernel::exported_flag_bits
     iree::compiler::Codegen::PassHeaders
     iree::compiler::Codegen::Utils
     iree::compiler::Dialect::Util::IR

compiler/src/iree/compiler/Codegen/VMVX/LowerLinalgMicrokernels.cpp
@@ -4,6 +4,8 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include "iree-dialects/Dialect/LinalgExt/IR/LinalgExtOps.h"
+#include "iree/builtins/ukernel/exported_flag_bits.h"
 #include "iree/compiler/Codegen/PassDetail.h"
 #include "iree/compiler/Codegen/Passes.h"
 #include "iree/compiler/Dialect/Util/IR/UtilDialect.h"
@@ -20,9 +22,6 @@
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassRegistry.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
-
-// TODO: move these flags to a header file shared with runtime/.
-#define IREE_VMVX_MATMUL_FLAG_ACCUMULATE 1
 
 namespace mlir {
 namespace iree_compiler {
@@ -888,6 +887,142 @@ struct LinalgFillConversion : public OpRewritePattern<linalg::FillOp> {
   }
 };
 
+struct LinalgExtPackConversion
+    : public OpRewritePattern<IREE::LinalgExt::PackOp> {
+  using OpRewritePattern::OpRewritePattern;
+  static bool isSupportedElementTypes(Type inElType, Type outElType) {
+    if (inElType.isF32() && outElType.isF32()) {
+      return true;
+    }
+    if (inElType.isSignlessInteger(8) && inElType.isSignlessInteger(8)) {
+      return true;
+    }
+    if (inElType.isSignlessInteger(32) && inElType.isSignlessInteger(32)) {
+      return true;
+    }
+    return false;
+  }
+
+  LogicalResult matchAndRewrite(IREE::LinalgExt::PackOp op,
+                                PatternRewriter &rewriter) const override {
+    MemRefType inType = op.getInputType().cast<MemRefType>();
+    MemRefType outType = op.getOutputType().cast<MemRefType>();
+
+    if (!isSupportedElementTypes(inType.getElementType(),
+                                 outType.getElementType())) {
+      return rewriter.notifyMatchFailure(
+          op, "unsupported combination of in/out element types");
+    }
+
+    if (inType.getRank() != 2) {
+      return rewriter.notifyMatchFailure(op, "expected input to be 2D");
+    }
+
+    if (outType.getRank() != 4) {
+      return rewriter.notifyMatchFailure(op, "expected output to be 4D");
+    }
+
+    int64_t innerDimsPos[2] = {0, 1};
+    if (ArrayAttr innerDimsPosAttr = op.getInnerDimsPosAttr()) {
+      innerDimsPos[0] = innerDimsPosAttr[0].cast<IntegerAttr>().getInt();
+      innerDimsPos[1] = innerDimsPosAttr[1].cast<IntegerAttr>().getInt();
+    }
+
+    int64_t outerDimsPerm[2] = {0, 1};
+    if (ArrayAttr outerDimsPermAttr = op.getOuterDimsPermAttr()) {
+      outerDimsPerm[0] = outerDimsPermAttr[0].cast<IntegerAttr>().getInt();
+      outerDimsPerm[1] = outerDimsPermAttr[1].cast<IntegerAttr>().getInt();
+    }
+
+    int flags = 0;
+
+    if (innerDimsPos[0] == 0 && innerDimsPos[1] == 1) {
+      // nothing to do
+    } else if (innerDimsPos[0] == 1 && innerDimsPos[1] == 0) {
+      flags |= IREE_UKERNEL_FLAG_PACK_TRANSPOSE_INNER;
+    } else {
+      return rewriter.notifyMatchFailure(op, "unsupported inner_dims_pos");
+    }
+
+    if (outerDimsPerm[0] == 0 && outerDimsPerm[1] == 1) {
+      // nothing to do
+    } else if (outerDimsPerm[0] == 1 && outerDimsPerm[1] == 0) {
+      flags |= IREE_UKERNEL_FLAG_PACK_TRANSPOSE_OUTER;
+    } else {
+      return rewriter.notifyMatchFailure(op, "unsupported outer_dims_perm");
+    }
+
+    SmallVector<int64_t> inStrides;
+    int64_t inOffset;
+    if (failed(mlir::getStridesAndOffset(inType, inStrides, inOffset))) {
+      return rewriter.notifyMatchFailure(
+          op, "failed to getStridesAndOffset for input");
+    }
+
+    if (inStrides[1] != 1) {
+      return rewriter.notifyMatchFailure(
+          op, "unsupported input layout with non-contiguous inner dimension");
+    }
+
+    if (!verifyMemRefInnerDimsContiguousRowMajor(outType)) {
+      return rewriter.notifyMatchFailure(
+          op,
+          "expected the output's inner dimensions to be contiguous row-major");
+    }
+
+    StridedBufferAnalysis inAnalysis(op.getInput());
+    StridedBufferAnalysis outAnalysis(op.getOutput());
+
+    if (!inAnalysis.isValid()) {
+      return rewriter.notifyMatchFailure(
+          op, "could not compute buffer descriptor for input");
+    }
+
+    if (!outAnalysis.isValid()) {
+      return rewriter.notifyMatchFailure(
+          op, "could not compute buffer descriptor for output");
+    }
+
+    Location loc = op.getLoc();
+
+    Value paddingValue = op.getPaddingValue();
+    if (!paddingValue) {
+      paddingValue = rewriter.create<arith::ConstantOp>(
+          loc, rewriter.getZeroAttr(inType.getElementType()),
+          inType.getElementType());
+    }
+
+    StridedBufferDescriptor &inDesc = inAnalysis.getDesc(rewriter);
+    StridedBufferDescriptor &outDesc = outAnalysis.getDesc(rewriter);
+
+    Value inBuffer = inDesc.castToLinear(loc, rewriter);
+    Value outBuffer = outDesc.castToLinear(loc, rewriter);
+    Value inSize0 = inDesc.sizes[0];
+    Value inSize1 = inDesc.sizes[1];
+    Value inStride0 = inDesc.strides[0];
+    Value outSize0 = outDesc.sizes[0];
+    Value outSize1 = outDesc.sizes[1];
+    Value outSize2 = outDesc.sizes[2];
+    Value outSize3 = outDesc.sizes[3];
+    Value outStride0 = outDesc.strides[0];
+
+    rewriter.replaceOpWithNewOp<IREE::VMVX::PackOp>(
+        op,
+        // input
+        inBuffer, inDesc.offset, inStride0,
+        // output
+        outBuffer, outDesc.offset, outStride0,
+        // input shape
+        inSize0, inSize1,
+        // output shape
+        outSize0, outSize1, outSize2, outSize3, paddingValue,
+        // flags
+        inDesc.getElementTypeAttr(), outDesc.getElementTypeAttr(),
+        rewriter.getI32IntegerAttr(flags));
+    return success();
+  }
+};
+
 bool isMmt4d(ArrayAttr indexingMaps) {
   if (indexingMaps.size() != 3) return false;
 
@@ -1049,7 +1184,7 @@ struct LinalgContractionConversion
     if (linalg::FillOp fillOp = findFillOpSolelyZeroingOutputOf(info.op)) {
       rewriter.eraseOp(fillOp);  // let the matmul overwrite the accumulator.
     } else {
-      flags |= IREE_VMVX_MATMUL_FLAG_ACCUMULATE;  // accumulate into existing.
+      flags |= IREE_UKERNEL_FLAG_ACCUMULATE;  // accumulate into existing.
     }
 
     auto &lhsDesc = info.lhsAnal.getDesc(rewriter);
@@ -1087,7 +1222,7 @@ struct LinalgContractionConversion
     if (linalg::FillOp fillOp = findFillOpSolelyZeroingOutputOf(info.op)) {
       rewriter.eraseOp(fillOp);  // let the matmul overwrite the accumulator.
     } else {
-      flags |= IREE_VMVX_MATMUL_FLAG_ACCUMULATE;  // accumulate into existing.
+      flags |= IREE_UKERNEL_FLAG_ACCUMULATE;  // accumulate into existing.
     }
 
     auto &lhsDesc = info.lhsAnal.getDesc(rewriter);
@@ -1152,10 +1287,10 @@ class VMVXLowerLinalgMicrokernelsPass
     // Other lowering patterns
     {
       RewritePatternSet patterns(&getContext());
-      patterns
-          .insert<LinalgBinaryGenericConversion, LinalgFillConversion,
-                  LinalgTrivialGenericConversion, LinalgUnaryGenericConversion>(
-              &getContext());
+      patterns.insert<LinalgBinaryGenericConversion, LinalgFillConversion,
+                      LinalgTrivialGenericConversion,
+                      LinalgUnaryGenericConversion, LinalgExtPackConversion>(
+          &getContext());
 
       if (failed(applyPatternsAndFoldGreedily(getOperation(),
                                               std::move(patterns)))) {

compiler/src/iree/compiler/Codegen/VMVX/test/lower_linalg_microkernels.mlir
@@ -559,3 +559,51 @@ func.func @rsqrt(%arg0 : memref<64x64xf32>, %arg1 : memref<64xf32>) {
   }
   func.return
 }
+
+// CHECK-LABEL: @pack_i8i8
+//   CHECK-DAG: %[[BB0:.*]], %[[OFFSET0:.*]], %[[SIZES0:.*]]:2, %[[STRIDES0:.*]]:2 = vmvx.get_buffer_descriptor %arg0
+//   CHECK-DAG: %[[BB1:.*]], %[[OFFSET1:.*]], %[[SIZES1:.*]]:4, %[[STRIDES1:.*]]:4 = vmvx.get_buffer_descriptor %arg1
+//       CHECK: vmvx.pack
+//  CHECK-SAME:   in(%[[BB0]] offset %[[OFFSET0]] stride0 %[[STRIDES0]]#0 : !util.buffer)
+//  CHECK-SAME:   out(%[[BB1]] offset %[[OFFSET1]] stride0 %[[STRIDES1]]#0 : !util.buffer)
+//  CHECK-SAME:   in_shape(%[[SIZES0]]#0, %[[SIZES0]]#1)
+//  CHECK-SAME:   out_shape(%[[SIZES1]]#0, %[[SIZES1]]#1, %[[SIZES1]]#2, %[[SIZES1]]#3)
+//  CHECK-SAME:   padding_value(%arg2 : i8)
+//  CHECK-SAME:   flags(0)
+func.func @pack_i8i8(%arg0 : memref<35x48xi8>, %arg1 : memref<5x6x7x8xi8>, %arg2 : i8) {
+  iree_linalg_ext.pack %arg0 padding_value(%arg2 : i8) inner_dims_pos = [0, 1] inner_tiles = [7, 8] into %arg1
+      : (memref<35x48xi8> memref<5x6x7x8xi8>)
+  func.return
+}
+
+// CHECK-LABEL: @pack_i32i32_transpose_inner_dims
+//   CHECK-DAG: %[[BB0:.*]], %[[OFFSET0:.*]], %[[SIZES0:.*]]:2, %[[STRIDES0:.*]]:2 = vmvx.get_buffer_descriptor %arg0
+//   CHECK-DAG: %[[BB1:.*]], %[[OFFSET1:.*]], %[[SIZES1:.*]]:4, %[[STRIDES1:.*]]:4 = vmvx.get_buffer_descriptor %arg1
+//       CHECK: vmvx.pack
+//  CHECK-SAME:   in(%[[BB0]] offset %[[OFFSET0]] stride0 %[[STRIDES0]]#0 : !util.buffer)
+//  CHECK-SAME:   out(%[[BB1]] offset %[[OFFSET1]] stride0 %[[STRIDES1]]#0 : !util.buffer)
+//  CHECK-SAME:   in_shape(%[[SIZES0]]#0, %[[SIZES0]]#1)
+//  CHECK-SAME:   out_shape(%[[SIZES1]]#0, %[[SIZES1]]#1, %[[SIZES1]]#2, %[[SIZES1]]#3)
+//  CHECK-SAME:   padding_value(%arg2 : i32)
+//  CHECK-SAME:   flags(65536)
+func.func @pack_i32i32_transpose_inner_dims(%arg0 : memref<35x48xi32>, %arg1 : memref<5x6x8x7xi32>, %arg2 : i32) {
+  iree_linalg_ext.pack %arg0 padding_value(%arg2 : i32) inner_dims_pos = [1, 0] inner_tiles = [8, 7] into %arg1
+      : (memref<35x48xi32> memref<5x6x8x7xi32>)
+  func.return
+}
+
+// CHECK-LABEL: @pack_f32f32_transpose_inner_and_outer_dims
+//   CHECK-DAG: %[[BB0:.*]], %[[OFFSET0:.*]], %[[SIZES0:.*]]:2, %[[STRIDES0:.*]]:2 = vmvx.get_buffer_descriptor %arg0
+//   CHECK-DAG: %[[BB1:.*]], %[[OFFSET1:.*]], %[[SIZES1:.*]]:4, %[[STRIDES1:.*]]:4 = vmvx.get_buffer_descriptor %arg1
+//       CHECK: vmvx.pack
+//  CHECK-SAME:   in(%[[BB0]] offset %[[OFFSET0]] stride0 %[[STRIDES0]]#0 : !util.buffer)
+//  CHECK-SAME:   out(%[[BB1]] offset %[[OFFSET1]] stride0 %[[STRIDES1]]#0 : !util.buffer)
+//  CHECK-SAME:   in_shape(%[[SIZES0]]#0, %[[SIZES0]]#1)
+//  CHECK-SAME:   out_shape(%[[SIZES1]]#0, %[[SIZES1]]#1, %[[SIZES1]]#2, %[[SIZES1]]#3)
+//  CHECK-SAME:   padding_value(%arg2 : f32)
+//  CHECK-SAME:   flags(196608)
+func.func @pack_f32f32_transpose_inner_and_outer_dims(%arg0 : memref<35x48xf32>, %arg1 : memref<6x5x8x7xf32>, %arg2 : f32) {
+  iree_linalg_ext.pack %arg0 padding_value(%arg2 : f32) outer_dims_perm = [1, 0] inner_dims_pos = [1, 0] inner_tiles = [8, 7] into %arg1
+      : (memref<35x48xf32> memref<6x5x8x7xf32>)
+  func.return
+}

compiler/src/iree/compiler/Dialect/VMVX/Conversion/VMVXToVM/ConvertVMVXToVM.cpp
@@ -188,6 +188,19 @@ class Mmt4dOpConversion : public VMVXImportOpConversion<IREE::VMVX::Mmt4dOp> {
   }
 };
 
+// Converts the vmvx.pack op to an appropriate typed import.
+class PackOpConversion : public VMVXImportOpConversion<IREE::VMVX::PackOp> {
+ public:
+  using VMVXImportOpConversion::VMVXImportOpConversion;
+
+  std::string getImportFqName(IREE::VMVX::PackOp op) const override {
+    std::string name("vmvx.pack.");
+    name.append(getTypedTypeStr(op.getInType()));
+    name.append(getTypedTypeStr(op.getOutType()));
+    return name;
+  }
+};
+
 class UnaryOpConversion : public VMVXImportOpConversion<IREE::VMVX::UnaryOp> {
  public:
   using VMVXImportOpConversion::VMVXImportOpConversion;
@@ -212,8 +225,8 @@ void populateVMVXToVMPatterns(MLIRContext *context,
                               SymbolTable &importSymbols,
                               RewritePatternSet &patterns) {
   patterns.insert<BinaryOpConversion, CopyOpConversion, Fill2DOpConversion,
-                  MatmulOpConversion, Mmt4dOpConversion, UnaryOpConversion>(
-      context, importSymbols, typeConverter);
+                  MatmulOpConversion, Mmt4dOpConversion, UnaryOpConversion,
+                  PackOpConversion>(context, importSymbols, typeConverter);
 }
 
 }  // namespace iree_compiler

compiler/src/iree/compiler/Dialect/VMVX/Conversion/VMVXToVM/test/BUILD
@@ -21,6 +21,7 @@ iree_lit_test_suite(
             "fill.mlir",
             "matmul.mlir",
             "mmt4d.mlir",
+            "pack.mlir",
             "unary.mlir",
         ],
         include = ["*.mlir"],

compiler/src/iree/compiler/Dialect/VMVX/Conversion/VMVXToVM/test/CMakeLists.txt
@@ -19,6 +19,7 @@ iree_lit_test_suite(
     "fill.mlir"
     "matmul.mlir"
     "mmt4d.mlir"
+    "pack.mlir"
     "unary.mlir"
   TOOLS
     FileCheck

commit 1f69f3ff43062afcef6bf191e5f2f3ea1114257b

Fixing MSVC breakage from #10969.

runtime/src/iree/builtins/ukernel/static_assert.h
@@ -22,7 +22,7 @@
 // here! It is defined by G++ but not by Clang++! So, let's also handle
 // __cplusplus before __STDC__.
 #define IREE_UKERNEL_STATIC_ASSERT(COND) static_assert(COND, #COND)
-#elif defined(__STDC__)
+#elif defined(__STDC__) || defined(_MSC_VER)
 // Really C, as neither __ASSEMBLER__ nor __cplusplus are defined.
 #define IREE_UKERNEL_STATIC_ASSERT(COND) _Static_assert(COND, #COND)
 #else