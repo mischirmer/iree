mischirmer/iree

commit b327dfd796869c764eb9646065301f6f6d7cad10

[CUDA] Remove call to nvvm-intr-range and add a pass setting ranges (#12904)
With the new pass manager nvvm-intr-range is disabled by default and the
new pass manager doesn't allow passing the shader model when adding this
pass. Therefore adding a new pass that allows to control the ranges
attached to blockid and grid size intrinsics. This allows llvm to do
arithmetic simplification.

compiler/src/iree/compiler/Dialect/HAL/Target/CUDA/CUDATarget.cpp
@@ -8,6 +8,7 @@
 
 #include "iree/compiler/Codegen/Dialect/IREECodegenDialect.h"
 #include "iree/compiler/Codegen/Passes.h"
+#include "iree/compiler/Dialect/HAL/Target/CUDA/LLVMPasses.h"
 #include "iree/compiler/Dialect/HAL/Target/LLVMLinkerUtils.h"
 #include "iree/compiler/Dialect/HAL/Target/TargetRegistry.h"
 #include "iree/compiler/Utils/FlatbufferUtils.h"
@@ -307,12 +308,8 @@ static void optimizeModule(llvm::Module &module,
   llvm::PassBuilder pb(&targetMachine, pto, std::nullopt, &pic);
   llvm::ModulePassManager mpm;
   StringRef nnvmReflectPassName = "nvvm-reflect";
-  StringRef nnvmIntrRangePassName = "nvvm-intr-range";
   if (pb.parsePassPipeline(mpm, nnvmReflectPassName)) {
     llvm::errs() << "Could not parse -" << nnvmReflectPassName << "\n";
-  }
-  if (pb.parsePassPipeline(mpm, nnvmIntrRangePassName)) {
-    llvm::errs() << "Could not parse -" << nnvmIntrRangePassName << "\n";
   }
   pb.registerModuleAnalyses(mam);
   pb.registerCGSCCAnalyses(cgam);
@@ -323,6 +320,9 @@ static void optimizeModule(llvm::Module &module,
   llvm::OptimizationLevel ol = llvm::OptimizationLevel::O2;
 
   mpm.addPass(llvm::VerifierPass());
+  llvm::FunctionPassManager fpm;
+  fpm.addPass(llvm::SetBlockIdsRangePass());
+  mpm.addPass(createModuleToFunctionPassAdaptor(std::move(fpm)));
   mpm.addPass(pb.buildPerModuleDefaultPipeline(ol));
   mpm.addPass(llvm::VerifierPass());
 

compiler/src/iree/compiler/Dialect/HAL/Target/CUDA/LLVMPasses.h
@@ -1,0 +1,23 @@
+// Copyright 2023 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_COMPILER_DIALECT_HAL_TARGET_CUDA_PASS_H_
+#define IREE_COMPILER_DIALECT_HAL_TARGET_CUDA_PASS_H_
+
+#include "llvm/IR/PassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/CodeGen.h"
+
+namespace llvm {
+
+/// Pass to set range metadata attached to block id intrinsics.
+struct SetBlockIdsRangePass : PassInfoMixin<SetBlockIdsRangePass> {
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+};
+
+}  // namespace llvm
+
+#endif  // IREE_COMPILER_DIALECT_HAL_TARGET_CUDA_PASS_H_

compiler/src/iree/compiler/Dialect/HAL/Target/CUDA/SetBlockIdsRangePass.cpp
@@ -1,0 +1,83 @@
+// Copyright 2023 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "iree/compiler/Dialect/HAL/Target/CUDA/LLVMPasses.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Intrinsics.h"
+#include "llvm/IR/IntrinsicsNVPTX.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/InitializePasses.h"
+#include "llvm/Pass.h"
+
+// This is a workaround until nvvm-intr-range gets re-enabled by default in
+// NVPTX backend. This also allows us to potentially special more the kernel by
+// setting more fine grain ranges based on static dispatch size.
+using namespace llvm;
+
+#define DEBUG_TYPE "iree-dialect-hal-cuda-llvm-set-block-ids-range"
+
+// Adds the passed-in [Low,High) range information as metadata to the
+// passed-in call instruction.
+static bool addRangeMetadata(uint64_t Low, uint64_t High, CallInst *C) {
+  // This call already has range metadata, nothing to do.
+  if (C->getMetadata(LLVMContext::MD_range)) return false;
+
+  LLVMContext &Context = C->getParent()->getContext();
+  IntegerType *Int32Ty = Type::getInt32Ty(Context);
+  Metadata *LowAndHigh[] = {
+      ConstantAsMetadata::get(ConstantInt::get(Int32Ty, Low)),
+      ConstantAsMetadata::get(ConstantInt::get(Int32Ty, High))};
+  C->setMetadata(LLVMContext::MD_range, MDNode::get(Context, LowAndHigh));
+  return true;
+}
+
+static bool runOnFunction(Function &F) {
+  bool Changed = false;
+  // We could use the number of block dispatched if it is known at compile time
+  // however this would prevent re-using kernel re-use. For now just use the API
+  // limit.
+  unsigned MaxGridSizeX = 0x7fffffff;
+  unsigned MaxGridSizeY = 0xffff;
+  unsigned MaxGridSizeZ = 0xffff;
+  for (Instruction &I : instructions(F)) {
+    CallInst *Call = dyn_cast<CallInst>(&I);
+    if (!Call) continue;
+    Function *Callee = Call->getCalledFunction();
+    if (!Callee) continue;
+    switch (Callee->getIntrinsicID()) {
+      // Index within grid
+      case Intrinsic::nvvm_read_ptx_sreg_ctaid_x:
+        Changed |= addRangeMetadata(0, MaxGridSizeX, Call);
+        break;
+      case Intrinsic::nvvm_read_ptx_sreg_ctaid_y:
+        Changed |= addRangeMetadata(0, MaxGridSizeY, Call);
+        break;
+      case Intrinsic::nvvm_read_ptx_sreg_ctaid_z:
+        Changed |= addRangeMetadata(0, MaxGridSizeZ, Call);
+        break;
+
+      // Grid size
+      case Intrinsic::nvvm_read_ptx_sreg_nctaid_x:
+        Changed |= addRangeMetadata(1, MaxGridSizeX + 1, Call);
+        break;
+      case Intrinsic::nvvm_read_ptx_sreg_nctaid_y:
+        Changed |= addRangeMetadata(1, MaxGridSizeY + 1, Call);
+        break;
+      case Intrinsic::nvvm_read_ptx_sreg_nctaid_z:
+        Changed |= addRangeMetadata(1, MaxGridSizeZ + 1, Call);
+        break;
+    }
+  }
+  return Changed;
+}
+
+PreservedAnalyses SetBlockIdsRangePass::run(Function &F,
+                                            FunctionAnalysisManager &AM) {
+  return runOnFunction(F) ? PreservedAnalyses::none()
+                          : PreservedAnalyses::all();
+}

compiler/src/iree/compiler/Dialect/HAL/Target/CUDA/BUILD.bazel
@@ -24,9 +24,11 @@ iree_compiler_cc_library(
     name = "CUDA",
     srcs = [
         "CUDATarget.cpp",
+        "SetBlockIdsRangePass.cpp",
     ],
     hdrs = [
         "CUDATarget.h",
+        "LLVMPasses.h",
     ],
     deps = [
         "//compiler/src/iree/compiler/Codegen",

compiler/src/iree/compiler/Dialect/HAL/Target/CUDA/CMakeLists.txt
@@ -19,8 +19,10 @@ iree_cc_library(
     CUDA
   HDRS
     "CUDATarget.h"
+    "LLVMPasses.h"
   SRCS
     "CUDATarget.cpp"
+    "SetBlockIdsRangePass.cpp"
   DEPS
     LLVMAnalysis
     LLVMBitReader

commit 46028eea732f6dcd3d572878f64a6ef4004f0ff6

[CUDA] Add range on thread ids in llvm ir (#12924)
This fixes some of the perf regression from
https://github.com/openxla/iree/commit/b327dfd796869c764eb9646065301f6f6d7cad10

compiler/src/iree/compiler/Dialect/HAL/Target/CUDA/CUDATarget.cpp
@@ -289,7 +289,8 @@ static LogicalResult linkObjects(Location loc, llvm::Module &module,
 
 /// Performs optimizations on |module| (including LTO-style whole-program ones).
 static void optimizeModule(llvm::Module &module,
-                           llvm::TargetMachine &targetMachine) {
+                           llvm::TargetMachine &targetMachine,
+                           const std::array<int32_t, 3> &maxWorkgroupSize) {
   llvm::LoopAnalysisManager lam;
   llvm::FunctionAnalysisManager fam;
   llvm::CGSCCAnalysisManager cgam;
@@ -321,7 +322,7 @@ static void optimizeModule(llvm::Module &module,
 
   mpm.addPass(llvm::VerifierPass());
   llvm::FunctionPassManager fpm;
-  fpm.addPass(llvm::SetBlockIdsRangePass());
+  fpm.addPass(llvm::SetBlockIdsRangePass(maxWorkgroupSize));
   mpm.addPass(createModuleToFunctionPassAdaptor(std::move(fpm)));
   mpm.addPass(pb.buildPerModuleDefaultPipeline(ol));
   mpm.addPass(llvm::VerifierPass());
@@ -523,8 +524,15 @@ class CUDATargetBackend final : public TargetBackend {
                           variantOp.getName(), ".linked.bc", *llvmModule);
       }
 
+      std::array<int32_t, 3> maxWorkgroupSize = {1, 1, 1};
+      for (int64_t i = 0, e = workgroupSizes.size(); i < e; i++) {
+        for (int64_t j = 0; j < maxWorkgroupSize.size(); j++) {
+          maxWorkgroupSize[j] =
+              std::max(maxWorkgroupSize[j], workgroupSizes[i][j]);
+        }
+      }
       // Run LTO-style full optimization on the linked modules.
-      optimizeModule(*llvmModule, *targetMachine);
+      optimizeModule(*llvmModule, *targetMachine, maxWorkgroupSize);
 
       // Dump bitcode post-linking and optimization.
       if (!options.dumpIntermediatesPath.empty()) {

compiler/src/iree/compiler/Dialect/HAL/Target/CUDA/LLVMPasses.h
@@ -15,7 +15,12 @@ namespace llvm {
 
 /// Pass to set range metadata attached to block id intrinsics.
 struct SetBlockIdsRangePass : PassInfoMixin<SetBlockIdsRangePass> {
+  SetBlockIdsRangePass(const std::array<int32_t, 3> &maxWorkgroupSize)
+      : maxWorkgroupSize(maxWorkgroupSize) {}
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+
+ private:
+  std::array<int32_t, 3> maxWorkgroupSize;
 };
 
 }  // namespace llvm

compiler/src/iree/compiler/Dialect/HAL/Target/CUDA/SetBlockIdsRangePass.cpp
@@ -36,7 +36,8 @@ static bool addRangeMetadata(uint64_t Low, uint64_t High, CallInst *C) {
   return true;
 }
 
-static bool runOnFunction(Function &F) {
+static bool runOnFunction(Function &F,
+                          const std::array<int32_t, 3> &maxWorkgroupSize) {
   bool Changed = false;
   // We could use the number of block dispatched if it is known at compile time
   // however this would prevent re-using kernel re-use. For now just use the API
@@ -50,6 +51,28 @@ static bool runOnFunction(Function &F) {
     Function *Callee = Call->getCalledFunction();
     if (!Callee) continue;
     switch (Callee->getIntrinsicID()) {
+      // Index within block
+      case Intrinsic::nvvm_read_ptx_sreg_tid_x:
+        Changed |= addRangeMetadata(0, maxWorkgroupSize[0], Call);
+        break;
+      case Intrinsic::nvvm_read_ptx_sreg_tid_y:
+        Changed |= addRangeMetadata(0, maxWorkgroupSize[1], Call);
+        break;
+      case Intrinsic::nvvm_read_ptx_sreg_tid_z:
+        Changed |= addRangeMetadata(0, maxWorkgroupSize[2], Call);
+        break;
+
+      // Block size
+      case Intrinsic::nvvm_read_ptx_sreg_ntid_x:
+        Changed |= addRangeMetadata(1, maxWorkgroupSize[0] + 1, Call);
+        break;
+      case Intrinsic::nvvm_read_ptx_sreg_ntid_y:
+        Changed |= addRangeMetadata(1, maxWorkgroupSize[1] + 1, Call);
+        break;
+      case Intrinsic::nvvm_read_ptx_sreg_ntid_z:
+        Changed |= addRangeMetadata(1, maxWorkgroupSize[2] + 1, Call);
+        break;
+
       // Index within grid
       case Intrinsic::nvvm_read_ptx_sreg_ctaid_x:
         Changed |= addRangeMetadata(0, MaxGridSizeX, Call);
@@ -78,6 +101,6 @@ static bool runOnFunction(Function &F) {
 
 PreservedAnalyses SetBlockIdsRangePass::run(Function &F,
                                             FunctionAnalysisManager &AM) {
-  return runOnFunction(F) ? PreservedAnalyses::none()
-                          : PreservedAnalyses::all();
+  return runOnFunction(F, maxWorkgroupSize) ? PreservedAnalyses::none()
+                                            : PreservedAnalyses::all();
 }